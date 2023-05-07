c424cadca7ae9cc1432d7fb65507e59b720f9a1d
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-05-07 23:36:44 +0200

    country list working

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,8 @@
 import Notiflix from 'notiflix';
-export const fetchCountries = name => {
-  return fetch(`https://restcountries.com/v3.1/name/{name}`)
+export { fetchCountries };
+
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
     .then(response => {
       // Sprawdzamy, czy odpowiedź jest poprawna (status HTTP 200-299). Jeśli nie, rzucony zostanie wyjątek z komunikatem, że odpowiedź sieciowa nie była poprawna. Jeśli odpowiedź jest poprawna w następnym kroku dane z serwera parsujemy za pomocą metody .json()
       if (!response.ok) {
@@ -10,16 +12,12 @@ export const fetchCountries = name => {
       //   Metoda response.json() przetwarza dane w formacie JSON zwrócone z serwera na obiekt JavaScript.Następnie te dane możemy wokorzystać w aplikacji do dalszych działań
       //   Funkcja zwraca obiekt obietnicy zawierający przetworzone dane.
     })
-    .than(data => {
-      // zostanie wywołana, gdy obietnica zostanie rozwiązana. W argumencie metody otrzymujemy przetworzone dane z serwera.
-      return data;
-    })
 
     .catch(error => {
       // - Metoda .catch() zostanie wywołana, gdy obietnica zostanie odrzucona. W argumencie metody otrzymujemy obiekt błędu.
       console.error('There was problem with the fetch operation: ', error);
       //   Metoda console.error() jest używana do wypisywania komunikatów o błędach. Komunikaty wypisywane za pomocą console.error() są wyświetlane w konsoli przeglądarki w sposób wyróżniający, zazwyczaj w kolorze czerwonym, co ułatwia ich zauważenie i identyfikację.
     });
-};
+}
 
 // Aby dowiedzieć się, czy obietnica została rozwiązana, możemy użyć metody .then() na zwróconej przez funkcję fetchCountries() obietnicy. Ta metoda będzie wywołana, gdy obietnica zostanie rozwiązana, i jako argument przyjmie wynik zwrócony przez obietnicę. Możemy również dodać blok .catch() do łapania ewentualnych błędów podczas wykonywania żądania HTTP.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,72 @@
-import 
-import  './css/styles.css';
-import {fetchcountries} from /fetchCountries.js 
-import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+import { debounce } from 'lodash';
 const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+function handleInput() {
+  //metoda trim() ucina niepotrzebne spacje na początku i końcu
+  const searchTerm = countryInput.value.trim();
+  console.log('search term', searchTerm);
+
+  if (searchTerm) {
+    fetchCountries(searchTerm)
+      .then(countries => {
+        // console.log(countries);
+        if (countries.length === 1) {
+          renderCountryInfo(countries[0]);
+        } else if (countries.length > 1 && countries.length <= 10) {
+          renderCountryList(countries);
+        } else if (countries.length > 10) {
+          Notiflix.Notify.info(
+            'too many matches found. Please enter a more specific name.'
+          );
+        } else {
+          Notiflix.Notify.failure(' OOops, there is no country with that name');
+        }
+      })
+      .catch(error => Notiflix.Notify.failure('something went wrong, sorry'));
+  } else {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+  }
+}
+
+const debouncedHandleInput = debounce(handleInput, DEBOUNCE_DELAY);
+
+// dodanie event listenera na zwykla metodę bez ograniczen wywolywania
+// countryInput.addEventListener('input', handleInput);
+
+// dodanie event listenera na metode z ograniczeniem wywolywania na DEBOUNCE_DELAY
+countryInput.addEventListener('input', debouncedHandleInput);
+
+function renderCountryList(countries) {
+  const similarCountries = countries
+    .map(({ name, flags }) => {
+      return `<li>
+          <h3 class="country_name">${name.official}</h3>
+          <img class="country_flag" src="${flags.png}" alt="${name.official} flag"/>
+        </li>`;
+    })
+    .join('');
+  countryList.innerHTML = similarCountries;
+}
+
+function renderCountryInfo(countries) {
+  const fullCountryInfo = countries
+    .map(({ name, capital, population, flags, languages }) => {
+      return `
+    <img class="country_flag" src="${flags.png}" alt="${name.official} flag"/>
+    <h1><span class="country_name">Country name:</span> ${name.official}<h1> 
+    <h3><b> Capital:</b> ${capital}</h3> 
+    <h3><b> Population: </b> ${population}</h3>
+    <h3><b> Languages: </b> ${languages}</h3>
+    `;
+    })
+    .join('');
+  countryInfo.innerHTML = fullCountryInfo;
+}
